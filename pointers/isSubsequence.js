52f2d186b1ae691b2ed776432f4f63249a87ab14
tamir.rust <timorust1@gmail.com>
2024-04-04 11:47:27 +0300

    392

---
+++ pointers/isSubsequence.js
@@ -0,0 +1,22 @@
+/**
+ * Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+
+A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isSubsequence = function (s, t) {
+	let i = 0
+	let j = 0
+
+	while (i < s.length && j < t.length) {
+		if (s.charAt(i) == t.charAt(j)) {
+			j++
+			i++
+		} else {
+			j++
+		}
+	}
+	return i == s.length
+}
